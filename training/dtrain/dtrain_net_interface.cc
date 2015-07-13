b6d4eef7d31e357bc59e40655123138806535704
Patrick Simianer <p@simianer.de>
2015-07-13 18:49:28 +0200

    more dense features

--- training/dtrain/dtrain_net_interface.cc
+++ training/dtrain/dtrain_net_interface.cc
@@ -218,7 +218,7 @@ main(int argc, char** argv)
 
     // debug --
     WriteFile f(debug_fn);
-    *f << debug_output.str();
+    f.get() << debug_output.str();
     // -- debug
 
     // write current weights
